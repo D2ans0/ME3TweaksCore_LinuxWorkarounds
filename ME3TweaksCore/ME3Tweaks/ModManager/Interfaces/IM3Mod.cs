70ce8467561d54bd7365f2432f5e4d93db697a53
Mgamerz <mgamerz@msn.com>
2025-11-23 14:53:14 -0700

    Add IDisplayableMod interface

--- ME3TweaksCore/ME3Tweaks/ModManager/Interfaces/IM3Mod.cs
+++ ME3TweaksCore/ME3Tweaks/ModManager/Interfaces/IM3Mod.cs
@@ -1,4 +1,5 @@
-﻿using ME3TweaksCore.Objects;
+﻿using LegendaryExplorerCore.Packages;
+using ME3TweaksCore.Objects;
 using System.Collections.Generic;
 
 namespace ME3TweaksCore.ME3Tweaks.ModManager.Interfaces
@@ -14,6 +15,21 @@ namespace ME3TweaksCore.ME3Tweaks.ModManager.Interfaces
         /// List of DLC required for this mod to be installed
         /// </summary>
         public List<DLCRequirement> RequiredDLC { get; set; }
+    }
 
+    /// <summary>
+    /// Interface for displaying a mod in a list
+    /// </summary>
+    public interface IDisplayableMod
+    {
+        /// <summary>
+        /// Name that can be displayed in the user interface
+        /// </summary>
+        public string DisplayName { get; }
+
+        /// <summary>
+        /// Game this mod is for
+        /// </summary>
+        public MEGame Game { get; }
     }
 }
